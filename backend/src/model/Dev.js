2142e5335ef4deb5b9a18e66d6b5b08351d78cca
Vinícius de Ataídes Bittencourt <vinisataides@gmail.com>
2019-10-01 22:24:19 -0300

    Models

--- backend/src/model/Dev.js
+++ backend/src/model/Dev.js
@@ -1,3 +1,24 @@
-const mongoose = require("mongoose");
+const { Schema, model } = require("mongoose");
 
-mongoose.Schema;
+const DevSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    user: {
+      type: String,
+      required: true
+    },
+    bio: String,
+    avatar: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = model("Dev", DevSchema);
