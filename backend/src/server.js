ccadf775c3f54d1e4fa8ba5405c0bd95d6f0206b
Vinícius de Ataídes Bittencourt <vinisataides@gmail.com>
2019-10-01 22:19:30 -0300

    Mongoose

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const routes = require("./routes");
 const server = express();
 
+mongoose.connect("url", {
+  useNewUrlParser: true
+});
+
+server.use(express.json());
 server.use(routes);
 
 server.listen(3333);
