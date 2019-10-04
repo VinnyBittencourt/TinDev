32b4dbd99e2c35f69a7edf043733ec32496fe4a5
Vinícius de Ataídes Bittencourt <vinisataides@gmail.com>
2019-10-03 23:03:36 -0300

    fetchin data from api to the list

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Main.css";
 
 import api from "../services/api";
@@ -8,6 +8,8 @@ import like from "../assets/like.svg";
 import dislike from "../assets/dislike.svg";
 
 export default function Main({ match }) {
+  const [users, setUsers] = useState([]);
+
   useEffect(() => {
     async function loadUsers() {
       const response = await api.get("/devs", {
@@ -15,7 +17,7 @@ export default function Main({ match }) {
           user: match.params.id
         }
       });
-      console.log(response.data);
+      setUsers(response.data);
     }
     loadUsers();
   }, [match.params.id]);
@@ -23,96 +25,23 @@ export default function Main({ match }) {
     <div className='main-container'>
       <img src={logo} alt='Tindev' />
       <ul>
-        <li>
-          <img
-            src='https://avatars0.githubusercontent.com/u/38366235?v=4'
-            alt=''
-          />
-          <footer>
-            <strong>Vinny Bittencourt</strong>
-            <p>Um programador full-stack que ama a sua namorada.</p>
-          </footer>
-          <div className='buttons'>
-            <button type='button'>
-              <img src={dislike} alt='dislike' />
-            </button>
-            <button type='button'>
-              <img src={like} alt='like' />
-            </button>
-          </div>
-        </li>
-        <li>
-          <img
-            src='https://avatars0.githubusercontent.com/u/38366235?v=4'
-            alt=''
-          />
-          <footer>
-            <strong>Vinny Bittencourt</strong>
-            <p>Um programador full-stack que ama a sua namorada.</p>
-          </footer>
-          <div className='buttons'>
-            <button type='button'>
-              <img src={dislike} alt='dislike' />
-            </button>
-            <button type='button'>
-              <img src={like} alt='like' />
-            </button>
-          </div>
-        </li>
-        <li>
-          <img
-            src='https://avatars0.githubusercontent.com/u/38366235?v=4'
-            alt=''
-          />
-          <footer>
-            <strong>Vinny Bittencourt</strong>
-            <p>Um programador full-stack que ama a sua namorada.</p>
-          </footer>
-          <div className='buttons'>
-            <button type='button'>
-              <img src={dislike} alt='dislike' />
-            </button>
-            <button type='button'>
-              <img src={like} alt='like' />
-            </button>
-          </div>
-        </li>
-        <li>
-          <img
-            src='https://avatars0.githubusercontent.com/u/38366235?v=4'
-            alt=''
-          />
-          <footer>
-            <strong>Vinny Bittencourt</strong>
-            <p>Um programador full-stack que ama a sua namorada.</p>
-          </footer>
-          <div className='buttons'>
-            <button type='button'>
-              <img src={dislike} alt='dislike' />
-            </button>
-            <button type='button'>
-              <img src={like} alt='like' />
-            </button>
-          </div>
-        </li>
-        <li>
-          <img
-            src='https://avatars0.githubusercontent.com/u/38366235?v=4'
-            alt=''
-          />
-          <footer>
-            <strong>Vinny Bittencourt</strong>
-            <p>Um programador full-stack que ama a sua namorada.</p>
-          </footer>
-          <div className='buttons'>
-            <button type='button'>
-              <img src={dislike} alt='dislike' />
-            </button>
-            <button type='button'>
-              <img src={like} alt='like' />
-            </button>
-          </div>
-        </li>
+        {users.map(user => (
+          <li key={user._id}>
+            <img src={user.avatar} alt={user.name} />
+            <footer>
+              <strong>{user.name}</strong>
+              <p>{user.bio}</p>
+            </footer>
+            <div className='buttons'>
+              <button type='button'>
+                <img src={dislike} alt='dislike' />
+              </button>
+              <button type='button'>
+                <img src={like} alt='like' />
+              </button>
+            </div>
+          </li>
+        ))}
       </ul>
     </div>
   );
