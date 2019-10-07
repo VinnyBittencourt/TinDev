8bfb2797f42e91dab68843632b5a6882d611e7b2
Vinícius de Ataídes Bittencourt <vinisataides@gmail.com>
2019-10-06 23:13:38 -0300

    AsyncStorage + Main page

--- tindevmobile/src/pages/Main.js
+++ tindevmobile/src/pages/Main.js
@@ -1,6 +1,55 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Image, StyleSheet, Text} from 'react-native';
+
+import logo from '../assets/logo.png';
+import {SafeAreaView} from 'react-navigation';
 
 export default function Main() {
-  return <View />;
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image source={logo} />
+
+      <View>
+        <View style={styles.cardsContainers}>
+          <View style={styles.card}>
+            <Image
+              source={{
+                uri: 'https://avatars0.githubusercontent.com/u/38366235?v=4',
+              }}
+            />
+            <View style={styles.footer}>
+              <Text style={styles.name}>Vinny Bittencourt</Text>
+              <Text style={styles.bio}>Programador Web and Mobile</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+
+      <View />
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+
+  cardsContainers: {
+    flex: 1,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    maxHeight: 500,
+  },
+
+  card: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 8,
+    margin: 30,
+    overflow: 'hidden',
+  },
+});
